6a06c6b9503f0abe9562b129a52e8ab1234d4e48
Erick Sosa Garcia <ericksosagarcias@gmail.com>
2020-01-21 16:54:03 -0400

    update controller and model for mvc

--- controller.py
+++ controller.py
@@ -1,53 +1,54 @@
 class Controller:
-    def token(self, token, color, canEat = False, dame = False, empty = None): # 'Create' token
-        self.token = token
-        self.color = color
-        self.canEat = canEat
-        self.dame = dame
-        self.empty = empty
-    def take_token(self, position_initial_x, position_initial_y): # Select a token whit the actual position
-        self.position_initial_x = position_initial_x
-        self.position_initial_y = position_initial_y
-    def is_dame(self): # If a token get the other side of the table becomes a dame
-        if self.position_initial_y == 0:
-            self.dame = True
-    def left_up_diagonal_move(self): # Move forward diagonal left
-        self.position_initial_x = self.position_inital_x - 1
-        self.position_initial_y = self.position_initial_y - 1
-    def rigth_up_diagonal_move(self): # Move forward diagonal rigth
-        self.position_initial_x = self.position_initial_x + 1
-        self.position_initial_y = self.position_initial_y - 1
-    def left_down_diagonal_move(self): # Move backward diagonal left
-        if self.dame == True:
-            self.position_initial_x = self.position_initial_x - 1
-            self.position_initial_y = self.position_initial_y + 1
-    def right_down_diagonal_move(self): # Move backward diagonal rigth
-        if self.dame == True:
-            self.position_initial_x = self.position_initial_x + 1
-            self.position_initial_y = self.position_initial_y + 1
-
-
-# class Controller:
-#     def __init__(self):
-#         self.cord_x1 = None
-#         self.cord_y1 = None
-#         self.cord_x2 = None
-#         self.cord_y2 = None
-#         self.view = None
-#         self.table = None
-#         self.token = None
-#     def get_moves_player(self, cord_x1 = 0, cord_y1 = 0, cord_x2 = 0, cord_y2 = 0):
-#         self.cord_x1 = cord_x1
-#         self.cord_x2 = cord_x2
-#         self.cord_y1 = cord_y1
-#         self.cord_y2 = cord_y2
-#     def get_tokens(self, table):
-#         self.table = table
-#     def return_moves(self):
-#         return self.cord_x1, self.cord_y1, self.cord_x2, self.cord_y2
-#     def return_render_view(self):
-#         return self.view
-#     def get_view_to_render(self, view):
-#         self.view = view
-#     def move_diagonal_up_left(self):
-#         pass
+    def __init__(self):
+        self.table = None
+        self.position_token_x = None
+        self.position_token_y = None
+        self.direction = None
+        self.token = None
+    def get_table(self, table): # Get the table
+        self.table = table
+    def __token_to_move(self, position_token_x, position_token_y, direction): # Select a token whit the actual position
+        self.position_token_x = int(position_token_x)
+        self.position_token_y = int(position_token_y)
+        self.direction = direction.lower()
+    def move_token(self):
+        self.__token_to_move(input(': '), input(': '), input('direction: '))
+        if self.valid_to_move():
+            if self.table[self.position_token_x][self.position_token_y] == '⚫' and self.next_not_block(self.direction):
+                if self.direction == 'l':
+                    return self.position_token_x, self.position_token_y, (self.position_token_x + 1), (self.position_token_y - 1)
+                elif self.direction == 'r':
+                    return self.position_token_x, self.position_token_y, (self.position_token_x + 1), (self.position_token_y + 1)
+            elif self.table[self.position_token_x][self.position_token_y] == '⭕' and self.next_not_block(self.direction):
+                if self.direction == 'l':
+                    return self.position_token_x, self.position_token_y, (self.position_token_x - 1), (self.position_token_y - 1)
+                elif self.direction == 'r':
+                    return self.position_token_x, self.position_token_y, (self.position_token_x - 1), (self.position_token_y + 1)
+    def next_not_block(self, direction):
+        if direction == 'r':
+            move = self.table[self.position_token_x + 1][self.position_token_y + 1]
+            if move == '⬛':
+                return True
+            if move == '⚫':
+                return False
+            if move == '⭕':
+                if self.table[self.position_token_x + 2][self.position_token_y + 2] == '⬛':
+                    return True
+                else:
+                    return False
+        elif direction == 'l':
+            move = self.table[self.position_token_x + 1][self.position_token_y - 1]
+            if move == '⬛':
+                return True
+            if move == '⚫':
+                return False
+            if move == '⭕':
+                if self.table[self.position_token_x + 2][self.position_token_y + 2] == '⬛':
+                    return True
+                else:
+                    return False
+    def valid_to_move(self):
+        if self.table[self.position_token_x][self.position_token_y] == '⬜' or self.table[self.position_token_x][self.position_token_y] == '⬛':
+            return False
+        else:
+            return True
--- damas.py
+++ damas.py
@@ -5,8 +5,8 @@ import time
 new_controller = Controller()
 new_model = Model()
 new_view = View()
-
-for i in range(10):
-    new_controller.get_moves_player(int(input('x1: ')), int(input('y1: ')), int(input('x2: ')), int(input('y2: ')))
-    new_model.input_instructions(new_controller.return_moves())
+for i in range(15):
+    new_controller.get_table(new_model.return_raw_table())
+    new_model.input_instructions(new_controller.move_token())
+    # new_model.tested()
     new_view.render_view(new_model.return_model_view())
