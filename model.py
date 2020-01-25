129f989e372e85a11a3c0535dfab735a8d7da97b
Erick Sosa Garcia <ericksosagarcias@gmail.com>
2020-01-25 16:12:47 -0400

    fixed

--- model.py
+++ model.py
@@ -1,14 +1,14 @@
 class Model:
     def __init__(self):
         self.Table = [
-            ['1','⬜', '⬛', '⬜', '⬛', '⬜', '⬛', '⬜' ,'⬛', '\n'],
-            ['2','⬛', '⬜', '⬛', '⬜', '⬛', '⬜', '⬛' ,'⬜', '\n'],
-            ['3','⬜', '⚫', '⬜', '⚫', '⬜', '⬛', '⬜' ,'⬛', '\n'],
-            ['4','⬛', '⬜', 'R', '⬜', '⬛', '⬜', 'R' ,'⬜', '\n'],
-            ['5','⬜', '⚫', '⬜', '⚫', '⬜', '⬛', '⬜' ,'⬛', '\n'],
-            ['6','⬛', '⬜', '⬛', '⬜', '⬛', '⬜', '⬛' ,'⬜', '\n'],
-            ['7','⬜', '⬛', '⬜', '⬛', '⬜', '⬛', '⬜' ,'⬛', '\n'],
-            ['8','⬛', '⬜', '⬛', '⬜', '⬛', '⬜', '⬛' ,'⬜', '\n'],
+            ['1','⬜', '⚫', '⬜', '⚫', '⬜', '⚫', '⬜' ,'⚫', '\n'],
+            ['2','⚫', '⬜', '⚫', '⬜', '⚫', '⬜', '⚫' ,'⬜', '\n'],
+            ['3','⬜', '⚫', '⬜', '⚫', '⬜', '⚫', '⬜' ,'⚫', '\n'],
+            ['4','⬛', '⬜', '⬛', '⬜', '⬛', '⬜', '⬛' ,'⬜', '\n'],
+            ['5','⬜', '⬛', '⬜', '⬛', '⬜', '⬛', '⬜' ,'⬛', '\n'],
+            ['6','⭕', '⬜', '⭕', '⬜', '⭕', '⬜', '⭕' ,'⬜', '\n'],
+            ['7','⬜', '⭕', '⬜', '⭕', '⬜', '⭕', '⬜' ,'⭕', '\n'],
+            ['8','⭕', '⬜', '⭕', '⬜', '⭕', '⬜', '⭕' ,'⬜', '\n'],
             ['',' ','A', 'B', 'C', 'D', 'E', 'F', 'G' ,'H', '\n']
         ]
         self.generated_table = []
@@ -23,13 +23,15 @@ class Model:
                 if len(instructions) == 5:
                     self.__token_black_instructions(instructions)
                     self.__token_red_instructions(instructions)
+                    self.__token_red_dame_instructions(instructions)
+                    self.__token_black_dame_instructions(instructions)
                 else:
                     self.Table[instructions[0]][instructions[1]] = '⬛'
                     if self.tokens_cache == '⭕' and instructions[2] == 0: # Add instructions convert a red token to dame
                         self.Table[instructions[2]][instructions[3]] = 'R'
                     elif self.tokens_cache == '⚫' and instructions[2] == 7: # Same whit black token
                         self.Table[instructions[2]][instructions[3]] = 'B'
-                    else:    
+                    else:
                         self.Table[instructions[2]][instructions[3]] = self.tokens_cache
             for j in self.Table:
                 for i in j:
@@ -59,7 +61,7 @@ class Model:
                         self.Table[instructions[0] + 1][instructions[1] + 1] = '⬛'
                         if instructions[2] == 7: # Add instrucction for convert to dame
                             self.Table[instructions[2]][instructions[3]] = 'B'
-                        else:    
+                        else:
                             self.Table[instructions[2]][instructions[3]] = self.tokens_cache
             if instructions[4] == 'l':
                 if self.Table[instructions[0] + 1][instructions[1] - 1] == '⭕' or self.Table[instructions[0] + 1][instructions[1] - 1] == 'R':
@@ -68,7 +70,7 @@ class Model:
                         self.Table[instructions[0] + 1][instructions[1] - 1] = '⬛'
                         if instructions[2] == 7: # Add instrucction for convert to dame
                             self.Table[instructions[2]][instructions[3]] = 'B'
-                        else:    
+                        else:
                             self.Table[instructions[2]][instructions[3]] = self.tokens_cache
     def __token_red_instructions(self, instructions):
         if self.tokens_cache == '⭕':
@@ -79,16 +81,16 @@ class Model:
                         self.Table[instructions[0] - 1][instructions[1] + 1] = '⬛'
                         if instructions[2] == 0: # Add instrucction for convert to dame
                             self.Table[instructions[2]][instructions[3]] = 'R'
-                        else:    
+                        else:
                             self.Table[instructions[2]][instructions[3]] = self.tokens_cache
             if instructions[4] == 'l':
-                if self.Table[instructions[0] - 1][instructions[1] - 1] == '⚫' or self.Table[instructions[0] - 1][instructions[1] - 1] == 'B': 
+                if self.Table[instructions[0] - 1][instructions[1] - 1] == '⚫' or self.Table[instructions[0] - 1][instructions[1] - 1] == 'B':
                     if self.Table[instructions[0] - 2][instructions[1] - 2] == '⬛':
                         self.Table[instructions[0]][instructions[1]] = '⬛'
                         self.Table[instructions[0] - 1][instructions[1] - 1] = '⬛'
                         if instructions[2] == 0: # Add instrucction for convert to dame
                             self.Table[instructions[2]][instructions[3]] = 'R'
-                        else:    
+                        else:
                             self.Table[instructions[2]][instructions[3]] = self.tokens_cache
     def __token_black_dame_instructions(self, instructions):
         if self.tokens_cache == 'B':
